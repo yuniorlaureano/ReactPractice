1011d065a1c51fdd6c2895f9cbabd2fcd0d9da11
= <=>
2019-02-03 11:41:46 -0400

    Configuracion del react con webpack y babel

---
+++ Ingredients/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/assets/'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
